451fdf10da75292f06d27c1ab44f4d3fb5d5b91a
Adesh Singh Rathore <72265093+GreyHat9@users.noreply.github.com>
2020-10-28 04:28:19 +0000

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,1 +1,1 @@
-sudo apt install -y git wget figlet toilet curl python python2 python3 python-pip python3-pip zip apache2 curl php nmap ruby
+sudo apt install -y git wget figlet toilet curl zip apache2 php nmap ruby python python2 python3 python-pip python3-pip
