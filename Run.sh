b082ddd01a3ae95cd3ec83b51551041f6da5f7b3
Adesh Singh Rathore <72265093+GreyHat9@users.noreply.github.com>
2021-01-10 15:00:09 +0000

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,1 +1,17 @@
-sudo apt install -y git wget figlet toilet curl zip apache2 php nmap ruby python python2 python3 python-pip python3-pip
+[+] sudo apt update
+[+] sudo apt install git
+[+] sudo apt install wget
+[+] sudo apt install figlet
+[+] sudo apt install toilet
+[+] sudo apt install curl
+[+] sudo apt install zip
+[+] sudo apt install apache2
+[+] sudo apt install php
+[+] sudo apt install nmap
+[+] sudo apt install ruby
+[+] sudo apt install python
+[+] sudo apt install python2
+[+] sudo apt install python3
+[+] sudo apt install python-pip
+[+] sudo apt install python3-pip
+[+] sudo gem install lolcat
